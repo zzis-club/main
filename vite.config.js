5bdfe26a69a93127d0991dc57d01b09a78dd0066
chiu20060726 <chiu20060726@gmail.com>
2022-08-25 01:27:15 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), Icons({ autoInstall: true, compiler: "vue3" })],
-    base: "main",
+    base: "/main/",
 });
