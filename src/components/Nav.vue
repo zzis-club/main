69b2c6bba41d82a61d72031191ec250760d90d05
chiu20060726 <chiu20060726@gmail.com>
2022-08-25 00:12:54 +0800

    a

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,7 @@
 <script setup>
 const links = [
-    { text: "Me", url: "/" },
-    { text: "About", url: "/about" },
-    { text: "Projects", url: "/projects" },
+    { text: "主頁面", url: "/" },
+    { text: "關於我們", url: "/about" },
 ];
 </script>
 
@@ -52,8 +51,8 @@ const links = [
                 v-if="$route.path !== '/about'"
             >
                 <img
-                    src=""
-                    class="w-full translate-y-12 scale-[180%] transition-all duration-200 hover:rounded-xl hover:pt-5 md:translate-y-14 md:hover:pt-7"
+                    src="https://cdn.discordapp.com/attachments/984715679682949130/1011856683405946890/home.png"
+                    class="h-full w-full"
                 />
             </div>
         </transition>
--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ const history = createWebHistory();
 const routes = [
     { path: "/", name: "home", component: () => import("./views/Home.vue") },
     { path: "/about", name: "about", component: () => import("./views/About.vue") },
-    { path: "/projects", name: "projects", component: () => import("./views/Projects.vue") },
     { path: "/:catchAll(.*)*", redirect: "/" },
 ];
 
